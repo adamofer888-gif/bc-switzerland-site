4b9715262a379314694b214ef6ddd097db312a98
adamofer888-gif <adamofer888@gmail.com>
2025-10-30 04:12:46 +0100

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,22 @@
+"use client";
+
+export default function Navbar() {
+  return (
+    <nav style={{
+      width: "100%",
+      padding: "20px",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      background: "#fff",
+      borderBottom: "1px solid #eee"
+    }}>
+      <h2>DTE HOLDING AG</h2>
+      <div style={{ display: "flex", gap: "20px" }}>
+        <a href="/">Home</a>
+        <a href="/services">Services</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
