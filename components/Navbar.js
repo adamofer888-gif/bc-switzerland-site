1beafca2eb6caf8e03a88865a7b5542620dc0f5b
adamofer888-gif <adamofer888@gmail.com>
2025-11-23 16:01:23 +0100

    style(navbar): create premium header for BC Switzerland

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,18 +4,28 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <header className="navbar">
-      <div className="navbar-container">
+      <div className="navbar-inner">
 
-        <div className="navbar-logo">
-          <Link href="/">BC Switzerland GmbH</Link>
-        </div>
+        {/* Logo + tagline */}
+        <Link href="/" className="navbar-brand">
+          <div className="brand-mark">BC</div>
+          <div className="brand-text">
+            <div className="brand-name">BC Switzerland GmbH</div>
+            <div className="brand-tagline">
+              Corporate advisory & Swiss structuring
+            </div>
+          </div>
+        </Link>
 
-        <nav className="navbar-links">
+        {/* Menu */}
+        <nav className="navbar-menu">
           <Link href="/">Accueil</Link>
           <Link href="/services">Services</Link>
           <Link href="/equipe">Équipe</Link>
           <Link href="/apropos">À propos</Link>
-          <Link href="/contact" className="nav-cta">Contact</Link>
+          <Link href="/contact" className="nav-btn">
+            Contact
+          </Link>
         </nav>
 
       </div>
